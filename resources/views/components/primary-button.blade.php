c1ce90c44f292f8dae2f027aa6422eff27a3a4f5
Arya Dharma <aryadharmawm22@gmail.com>
2024-10-27 00:13:39 +0800

    buat button primary

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,7 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge([
+    'type' => 'submit',
+    'class' => 'relative font-sm -top-1 -left-1 bg-gray-800 py-1.5 px-5 text-white transition-all active:top-0 active:left-0 active:bg-gray-900 before:content-[""] before:absolute before:top-1 before:left-1 before:w-full before:h-full before:border-2 before:border-gray-800 before:-z-10 before:transition-all active:before:top-0 active:before:left-0'
+]) }}>
     {{ $slot }}
 </button>
+
